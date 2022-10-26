23d01b87b589ad6f67eaeece97f1fc047f039518
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-10-26 19:59:30 -0300

    Livro CBB (2010): Atividade prática 5

---
+++ CBBAtividadePratica5.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePratica5 {
+    public static void main(String[] args){
+        int numero;
+        for(numero = 0; numero <= 100; numero ++){
+            if (numero % 2 == 0){
+                System.out.println(numero);
+            }
+        }
+    }
+}
