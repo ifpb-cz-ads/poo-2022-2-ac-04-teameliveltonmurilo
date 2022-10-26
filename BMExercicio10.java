62188f17af9dff0844f6a45d30108529f34c830f
livelton <105663496+livelton@users.noreply.github.com>
2022-10-26 19:58:21 -0300

    Livro Batista & Moraes (2013): Questão 10

---
+++ BMExercicio10.java
@@ -0,0 +1,13 @@
+public class BMExercicio10{
+    public static void main(String[] args){
+        int i = 0;
+        int x = 10;
+
+        do{
+            System.out.println(x);
+            x++;
+            i++;
+
+        } while(i <= 20);
+    }
+}
