febee42141da35ed28ce94f9eb8e1192a1e31bfc
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-10-26 20:12:30 -0300

    Livro Batista & Moraes (2013): Questão 6

---
+++ BMExercio06.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BMExercio06{
+    public static void main(String[] args){
+        Scanner entradaUsuario = new Scanner(System.in);
+        int numeroDoDia = 1;
+
+        while (numeroDoDia != 0){
+            System.out.println("Digite um número para o dia da semana (0 para sair): ");
+            numeroDoDia = entradaUsuario.nextInt();
+            switch(numeroDoDia){
+                case 1:
+                    System.out.println("O dia é Domingo");
+                    break;
+                case 2:
+                    System.out.println("O dia é Segunda");
+                    break;
+                case 3:
+                    System.out.println("O dia é Terça");
+                    break;
+                case 4:
+                    System.out.println("O dia é Quarta");
+                    break;
+                case 5:
+                    System.out.println("O dia é Quinta");
+                    break;
+                case 6:
+                    System.out.println("O dia é Sexta");
+                    break;
+                case 7:
+                    System.out.println("O dia é Sábado");
+                    break;
+                default:
+                    System.out.println("Número inválido");
+        }
+        }
+    }
+}
