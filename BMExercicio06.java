89e99a3fe406db21563ec0e05f10abaf411d0789
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-10-26 20:14:06 -0300

    Livro Batista & Moraes (2013): Questão 6
    Corrigindo o nome do arquivo

--- BMExercicio06.java
+++ BMExercicio06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BMExercio06{
+public class BMExercicio06{
     public static void main(String[] args){
         Scanner entradaUsuario = new Scanner(System.in);
         int numeroDoDia = 1;
