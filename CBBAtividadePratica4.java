0417bd9cf10c751658b96fc3e38d2e34632f1387
livelton <105663496+livelton@users.noreply.github.com>
2022-10-26 19:47:38 -0300

    Livro CBB (2010): Atividade prática 4

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePratica4 {
+    public static void main(String[] args){
+        int numero;
+
+        for(numero = 0; numero <= 100; numero ++){
+            System.out.println(numero);
+        }
+        
+    }
+}
